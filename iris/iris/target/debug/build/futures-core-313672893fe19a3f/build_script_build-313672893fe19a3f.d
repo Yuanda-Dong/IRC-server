3b644f9d180d1c427b6ab2e5a5cde8062157f4a1
Yuanda Dong <dongyuanda@gmail.com>
2023-05-03 00:28:13 +1000

    Cleanup Repo for gitFront

--- iris/iris/target/debug/build/futures-core-313672893fe19a3f/build_script_build-313672893fe19a3f.d
+++
@@ -1,8 +0,0 @@
-/home/me/Study/Rust/A2/iris/iris/target/debug/build/futures-core-313672893fe19a3f/build_script_build-313672893fe19a3f: /home/me/.cargo/registry/src/github.com-1ecc6299db9ec823/futures-core-0.3.25/build.rs /home/me/.cargo/registry/src/github.com-1ecc6299db9ec823/futures-core-0.3.25/no_atomic_cas.rs
-
-/home/me/Study/Rust/A2/iris/iris/target/debug/build/futures-core-313672893fe19a3f/build_script_build-313672893fe19a3f.d: /home/me/.cargo/registry/src/github.com-1ecc6299db9ec823/futures-core-0.3.25/build.rs /home/me/.cargo/registry/src/github.com-1ecc6299db9ec823/futures-core-0.3.25/no_atomic_cas.rs
-
-/home/me/.cargo/registry/src/github.com-1ecc6299db9ec823/futures-core-0.3.25/build.rs:
-/home/me/.cargo/registry/src/github.com-1ecc6299db9ec823/futures-core-0.3.25/no_atomic_cas.rs:
-
-# env-dep:CARGO_PKG_NAME=futures-core
--- iris/iris/target/debug/build/syn-0d4faf1e713d291f/build_script_build-0d4faf1e713d291f.d
+++
@@ -1,5 +0,0 @@
-/home/me/Study/Rust/A2/iris/iris/target/debug/build/syn-0d4faf1e713d291f/build_script_build-0d4faf1e713d291f: /home/me/.cargo/registry/src/github.com-1ecc6299db9ec823/syn-1.0.103/build.rs
-
-/home/me/Study/Rust/A2/iris/iris/target/debug/build/syn-0d4faf1e713d291f/build_script_build-0d4faf1e713d291f.d: /home/me/.cargo/registry/src/github.com-1ecc6299db9ec823/syn-1.0.103/build.rs
-
-/home/me/.cargo/registry/src/github.com-1ecc6299db9ec823/syn-1.0.103/build.rs:
